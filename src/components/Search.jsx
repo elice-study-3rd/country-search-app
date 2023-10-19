bb33d61e22ad729e595caf4e554433ef55e1585c
jieun <balllling92@gmail.com>
2023-10-20 02:37:00 +0900

    :sparkles: FEAT: added findCountriesByName function
    input value값과 일부 일치하는 모든 국가를 반환하는 기능 추가

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,44 @@
 import "../styles/Search.css"
 import { Icon } from '@iconify/react';
+import { useEffect, useState } from "react";
+
+import { Fetcher } from "../api/fetch";
 
 const Search = () => {
 
+    const [countriesByName, setCountriesByName] = useState(null);
+    const [keyword, setKeyword] = useState("");
+    const [keywordArguments, setKeywordArguments] = useState("");
+
+    const fetcher = new Fetcher();
+
+    // input onChange 핸들러
+    const keywordChangeHandler = (e) => {
+        setKeyword(e.target.value);
+    }
+
+    // form onSubmit 핸들러
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        setKeywordArguments(keyword);
+    }
+
+    // input value값과 일부 일치하는 모든 국가 반환
+    useEffect(() => {
+        const findCountriesByName = async () => {
+            if(keywordArguments !== "") {
+                const result = await fetcher
+                    .searchCountriesByName(keywordArguments);
+                setCountriesByName(result);
+            }
+        }
+        findCountriesByName();
+    }, [keywordArguments]);
+
     return (
         <div className="searchContainer">
-            <form className="searchForm">
-                <button type="submit" className="submitButton" >
+            <form className="searchForm" onSubmit={onSubmitHandler}>
+                <button type="submit" className="submitButton">
                     <Icon
                         icon="tabler:search"
                         color="#777"
@@ -16,6 +48,8 @@ const Search = () => {
                 </button>
                 <input
                     type="text"
+                    value={keyword}
+                    onChange={keywordChangeHandler}
                     className="searchInput"
                     placeholder="Search for country..."
                 />
