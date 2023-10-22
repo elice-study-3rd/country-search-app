faff89a5a8bb03d9146a74e5d3a85506e7598eed
jihoon <lanslot84@gmail.com>
2023-10-22 12:21:33 +0900

    ♻️ REFACTOR : router 에러 케이스 및 detail 분기 추가

--- src/router/router.js
+++ src/router/router.js
@@ -6,30 +6,53 @@ const fetcher = new Fetcher();
  * URL path에 따라 useQuery에 보낼 함수를 분기처리하는 간이 router
  * 1) index인 경우 : renderPath가 없음
  *    e.g. http://localhost:3000/ => ['']
- * 2) 특정 국가의 full-name인 경우 : renderPath가 해당 국가명
- *    e.g. http://localhost:3000/india => ['india']
+ * 2) 특정 국가의 full-name인 경우 : renderPath가 deatil {full-name}으로 분리
+ *    e.g. http://localhost:3000/detail/india => ['detail', 'india']
  * 3) /search?query={name}이 있는 경우 : renderPath가 search, {name}으로 분리
  *    e.g. http://localhost:3000/search?q=korea => ['search', 'korea']
  * 4) /region?query={regionName}이 있는 경우 : renderPath가 region, {regionName}으로 분리
  *    e.g. http://localhost:3000/region?q=europe => ['region', 'europe']
  */
-function contentRouter (path) {
+async function contentRouter (path) {
 
-    let renderPath = path[path.length-1].split('?q=');
+    let renderPath = [];
+    let result = [];
+
+    if(path.join().includes("detail")) {
+        renderPath = path[path.length-1].split('/');
+    } else if (path.join().includes("search") || path.join().includes("region")) {
+        renderPath =  path[path.length-1].split('?q=');
+    } else {
+        renderPath = path[path.length-1].split('/');
+    }
 
     if(renderPath.length === 1) {
-        if(renderPath[0] === "") {
+        if( renderPath[0] === "") {
             return fetcher.fetchAllCountries();
         } else {
             return fetcher.searchCountryByFullName(renderPath[0]);
         }
     } else if(renderPath.length > 1) {
         if(renderPath[0] === "search") {
-            return fetcher.searchCountriesByName(renderPath[1]);
+            try {
+                result = await fetcher.searchCountriesByName(renderPath[1]);
+            } catch(error) {
+                if (error.message === "404") {
+                    result = [];
+                }
+            }
         } else if(renderPath[0] === "region"){
-            return fetcher.searchCountriesByRegion(renderPath[1]);
+            try {
+                result = await fetcher.searchCountriesByRegion(renderPath[1]);
+            } catch(error) {
+                if (error.message === "404") {
+                    result = [];
+                }
+            }
         } 
     }
+
+    return result;
 }
 
-export default contentRouter;
+export { contentRouter };
