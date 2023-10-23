9631c6aba987950fa568bd48c526b5887efcf22c
young <rhsiddlsidhd@nate.com>
2023-10-23 22:48:03 +0900

    :sparkles: Feat: 표시 순서 변경 기능 구현(국가명,인구)
    Related to: #8

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,7 @@ import { Fetcher } from "../api/fetch";
 import { CountryCard } from "../components/CountryCard";
 import { useState } from "react";
 import "../styles/Main.css";
-import "../styles/DropDownMenu.css";
+import "../styles/Menu.css";
 
 const Main = () => {
   const fetcher = new Fetcher();
@@ -11,69 +11,134 @@ const Main = () => {
     fetcher.fetchAllCountries()
   );
 
-  console.log(data);
+  // console.log(data);
 
-  // 토글 상태
   const [view, setView] = useState(false);
 
-  const Dropdown = () => {
-    return (
-      <ul className="Dropdown_list">
-        <li id="list_Africa">Filter by Region</li>
-        <li id="list_America">Filter by Independent</li>
-      </ul>
-    );
+  const [sortType, setSortType] = useState(null);
+
+  const sortData = (type) => {
+    if (type === sortType) {
+      setSortType(null);
+    } else {
+      setSortType(type);
+    }
+  };
+
+  const sortedNations = () => {
+    if (sortType === "국가명 오름차순") {
+      return [...data].sort((a, b) => {
+        const nameA = a?.name?.common.toUpperCase();
+        const nameB = b?.name?.common.toUpperCase();
+        return nameA.localeCompare(nameB);
+      });
+    }
+    if (sortType === "국가명 내림차순") {
+      return [...data].sort((a, b) => {
+        const nameA = a?.name?.common.toUpperCase();
+        const nameB = b?.name?.common.toUpperCase();
+        return nameB.localeCompare(nameA);
+      });
+    }
+    if (sortType === "인구수 오름차순") {
+      return [...data].sort((a, b) => a.population - b.population);
+    }
+    if (sortType === "인구수 내림차순") {
+      return [...data].sort((a, b) => b.population - a.population);
+    }
+    return data; // 기본값
   };
 
   if (isLoading) {
-    //로딩창
+    // 로딩창 렌더링
   } else if (isError) {
-    //에러
+    // 에러 메시지 렌더링
   } else {
     return (
       <main>
         <header>{/* 검색창과 드롭다운 메뉴 */}</header>
-        <div className="dropDown_wrapping">
-          <div
-            className="DropDown_Btn"
-            onClick={() => {
-              setView(!view);
-            }}
-          >
-            <div>Filter</div>
-            {view ? (
-              <i className="fa-solid fa-chevron-down"></i>
-            ) : (
-              <i className="fa-solid fa-chevron-up"></i>
+        <div className="menu">
+          <div className="dropdown-menu">
+            <div
+              className="dropdown-button"
+              onClick={() => {
+                setView(!view);
+              }}
+            >
+              <p>Filter</p>
+              {view ? (
+                <i className="fa-solid fa-chevron-down"></i>
+              ) : (
+                <i className="fa-solid fa-chevron-up"></i>
+              )}
+            </div>
+            {view && (
+              <ul className="dropdown_list">
+                <li>Filter by Region</li>
+                <li>Filter by Independent</li>
+              </ul>
             )}
           </div>
-          {view && <Dropdown />}
-          <div className="Align">
-            <input type="radio" name="Align_list" id="Align_list1"></input>
-            <label for="Align_list1">국가명 오름차순</label>
-            <input type="radio" name="Align_list" id="Align_list2"></input>
-            <label for="Align_list2">국가명 내림차순</label>
-            <input type="radio" name="Align_list" id="Align_list3"></input>
-            <label for="Align_list3">인구수 오름차순</label>
-            <input type="radio" name="Align_list" id="Align_list4"></input>
-            <label for="Align_list4">인구수 내림차순</label>
+          <div className="sort-menu">
+            <div>
+              <input
+                type="radio"
+                name="align_list"
+                id="align_list1"
+                onClick={() => sortData("국가명 오름차순")}
+                checked={sortType === "국가명 오름차순"}
+                onChange={() => {}}
+              />
+              <label htmlFor="align_list1">국가명 오름차순</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="align_list"
+                id="align_list2"
+                onClick={() => sortData("국가명 내림차순")}
+                checked={sortType === "국가명 내림차순"}
+                onChange={() => {}}
+              />
+              <label htmlFor="align_list2">국가명 내림차순</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="align_list"
+                id="align_list3"
+                onClick={() => sortData("인구수 오름차순")}
+                checked={sortType === "인구수 오름차순"}
+                onChange={() => {}}
+              />
+              <label htmlFor="align_list3">인구수 오름차순</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="align_list"
+                id="align_list4"
+                onClick={() => sortData("인구수 내림차순")}
+                checked={sortType === "인구수 내림차순"}
+                onChange={() => {}}
+              />
+              <label htmlFor="align_list4">인구수 내림차순</label>
+            </div>
           </div>
         </div>
         <article className="cardList">
-          {data.map((eachCountry) => {
-            return (
-              <CountryCard
-                key={eachCountry.cca2}
-                imageUrl={eachCountry.flags.svg}
-                countryName={eachCountry.name.common}
-                population={eachCountry.population
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                region={eachCountry.region}
-                capital={eachCountry.capital}
-              ></CountryCard>
-            );
-          })}
+          {sortedNations().map((eachCountry) => (
+            <CountryCard
+              key={eachCountry.cca2}
+              imageUrl={eachCountry.flags.svg}
+              countryName={eachCountry.name.common}
+              population={eachCountry.population
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              region={eachCountry.region}
+              capital={eachCountry.capital}
+            />
+          ))}
         </article>
       </main>
     );
